13d4e2c48a08b9b411b7d69265cb74561c85b897
Mauricio Ortiz <mraortiz@hotmail.com>
2019-05-25 05:56:56 -0300

    added svg icons

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 let test = document.getElementsByClassName("test")[0];
 
 let h = window.getComputedStyle(test).height;
+let w = window.getComputedStyle(test).width;
 
-console.log("height = " + h);
+console.log("height = " + h + " " + "width = " + w);
