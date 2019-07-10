04a319521ac06f1a5f20ff0afaa0b7db909a0dc8
notebook <mraortiz@hotmail.com>
2019-07-10 05:49:51 -0300

    added get request for current and forecast weather

--- index.js
+++ index.js
@@ -10,15 +10,24 @@ app.use(express.static("public"));
 app.use(express.json({ limit: "1mb" }));
 
 app.get("/weather/:coords", async (request, response) => {
-    console.log(request.params);
+    //console.log(request.params);
     const coords = request.params.coords.split(",");
-    console.log(coords);
+    //console.log(coords);
     const lat = coords[0];
     const lon = coords[1];
-    console.log(lat, lon);
-    const api_url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=446355c29c56f4b0eaf41493d1017d93&units=metric`;
+    //console.log(lat, lon);
+    const currentWeather = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=446355c29c56f4b0eaf41493d1017d93&units=metric`;
 
-    const fetch_response = await fetch(api_url);
-    const response_json = await fetch_response.json();
-    response.json(response_json);
+    const fetch_currentWeather = await fetch(currentWeather);
+    const currentWeather_json = await fetch_currentWeather.json();
+
+    const forecastWeather = `https://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lon}&appid=446355c29c56f4b0eaf41493d1017d93&units=metric`;
+
+    const fetch_forecastWeather = await fetch(forecastWeather);
+    const forecastWeather_json = await fetch_forecastWeather.json();
+
+    response.json({
+        current: currentWeather_json,
+        forecast: forecastWeather_json
+    });
 });
